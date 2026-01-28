978651911fa83ac4ceebffb416939ca3769a123b
kepler51D-kys <Anonymous5939@protonmail.com>
2026-01-28 14:44:23 +0000

    rawr

---
+++ src/voxel.rs
@@ -0,0 +1,226 @@
+use bevy::{ecs::{component::Component, system::{Commands, Query}}, math::{IVec3, UVec3, Vec3}, mesh::Mesh, platform::collections::HashMap};
+
+#[repr(u16)]
+#[derive(PartialEq, Eq, Clone, Copy)]
+pub enum BlockID {
+    Air,
+    Stone,
+    Block
+}
+#[repr(u8)]
+pub enum ChunkState {
+    Renderable,
+    MeshRendering,
+    MeshDirty,
+    UnLoaded,
+}
+
+#[derive(Component)]
+pub struct Position(IVec3);
+
+#[derive(Component)]
+pub struct Chunk {
+    pub data: [
+        [
+            [
+                BlockID;Chunk::CHUNKSIZE
+            ];Chunk::CHUNKSIZE
+        ];Chunk::CHUNKSIZE
+    ],
+    pub model: Mesh,
+    pub state: ChunkState,
+}
+impl Chunk {
+    pub const CHUNKSIZE: usize = 32;
+    pub const TOPQUAD: [Vec3; 4] = [
+        Vec3 {x: 0.0, y: 1.0, z: 0.0},
+        Vec3 {x: 1.0, y: 1.0, z: 0.0},
+        Vec3 {x: 0.0, y: 1.0, z: 1.0},
+        Vec3 {x: 1.0, y: 1.0, z: 1.0},
+    ];
+    pub const BOTTOMQUAD: [Vec3; 4] = [
+        Vec3 {x: 0.0, y: 0.0, z: 0.0},
+        Vec3 {x: 0.0, y: 0.0, z: 1.0},
+        Vec3 {x: 1.0, y: 0.0, z: 0.0},
+        Vec3 {x: 1.0, y: 0.0, z: 1.0},
+    ];
+    pub const LEFTQUAD: [Vec3; 4] = [
+        Vec3 {x: 0.0, y: 0.0, z: 0.0},
+        Vec3 {x: 0.0, y: 1.0, z: 0.0},
+        Vec3 {x: 0.0, y: 0.0, z: 1.0},
+        Vec3 {x: 0.0, y: 1.0, z: 1.0},
+    ];
+    pub const RIGHTQUAD: [Vec3; 4] = [
+        Vec3 {x: 1.0, y: 0.0, z: 0.0},
+        Vec3 {x: 1.0, y: 0.0, z: 1.0},
+        Vec3 {x: 1.0, y: 1.0, z: 0.0},
+        Vec3 {x: 1.0, y: 1.0, z: 1.0},
+    ];
+    pub const FRONTQUAD: [Vec3; 4] = [
+        Vec3 {x: 0.0, y: 0.0, z: 1.0},
+        Vec3 {x: 0.0, y: 1.0, z: 1.0},
+        Vec3 {x: 1.0, y: 0.0, z: 1.0},
+        Vec3 {x: 1.0, y: 1.0, z: 1.0},
+    ];
+    pub const BACKQUAD: [Vec3; 4] = [
+        Vec3 {x: 1.0, y: 1.0, z: 0.0},
+        Vec3 {x: 1.0, y: 0.0, z: 0.0},
+        Vec3 {x: 0.0, y: 1.0, z: 0.0},
+        Vec3 {x: 0.0, y: 0.0, z: 0.0},
+    ];
+}
+pub struct ChunkManager {
+    pub map: HashMap<IVec3,Chunk>,
+}
+impl ChunkManager {
+    pub fn gen_mesh(&mut self, pos: IVec3) {
+        let chunk = match self.map.get_mut(&pos) {
+            Some(val) => {val},
+            None => {todo!()}
+        };
+        let chunk_data = chunk.data;
+        
+        let mut vertices: Vec<Vec3> = Vec::new();
+        // let mut normals: Vec<Vec3> = Vec::new();
+        let mut indices: Vec<u32> = Vec::new();
+        
+        chunk.state = ChunkState::MeshRendering;
+        for x in 0..Chunk::CHUNKSIZE {
+            for y in 0..Chunk::CHUNKSIZE {
+                for z in 0..Chunk::CHUNKSIZE {
+                    let offset = Vec3 {
+                        x: x as f32 + pos.x as f32,
+                        y: y as f32 + pos.y as f32,
+                        z: z as f32 + pos.z as f32,
+                    };
+                    if chunk_data[x][y][z] != BlockID::Air {
+                        if self.get_block(IVec3 {
+                            x:x as i32 -1,
+                            y:y as i32,
+                            z:z as i32
+                        }) == BlockID::Air {
+                            let new_quad: &mut [Vec3] = &mut Chunk::LEFTQUAD.clone();
+                            for i in 0..4 {
+                                new_quad[i] += offset;
+                            }
+                            indices.extend_from_slice(&[0,3,1,0,2,3]);
+                            vertices.extend_from_slice(new_quad);
+                        }
+                        if self.get_block(IVec3 {
+                            x:x as i32 +1,
+                            y:y as i32,
+                            z:z as i32
+                        }) == BlockID::Air {
+                            let new_quad: &mut [Vec3] = &mut Chunk::RIGHTQUAD.clone();
+                            for i in 0..4 {
+                                new_quad[i] += offset;
+                            }
+                            indices.extend_from_slice(&[0,3,1,0,2,3]);
+                            vertices.extend_from_slice(new_quad);
+                        }
+                        if self.get_block(IVec3 {
+                            x:x as i32,
+                            y:y as i32 -1,
+                            z:z as i32
+                        }) == BlockID::Air {
+                            let new_quad: &mut [Vec3] = &mut Chunk::BOTTOMQUAD.clone();
+                            for i in 0..4 {
+                                new_quad[i] += offset;
+                            }
+                            indices.extend_from_slice(&[0,3,1,0,2,3]);
+                            vertices.extend_from_slice(new_quad);
+                        }
+                        if self.get_block(IVec3 {
+                            x:x as i32,
+                            y:y as i32 +1,
+                            z:z as i32
+                        }) == BlockID::Air {
+                            let new_quad: &mut [Vec3] = &mut Chunk::TOPQUAD.clone();
+                            for i in 0..4 {
+                                new_quad[i] += offset;
+                            }
+                            indices.extend_from_slice(&[0,3,1,0,2,3]);
+                            vertices.extend_from_slice(new_quad);
+                        }
+                        if self.get_block(IVec3 {
+                            x:x as i32,
+                            y:y as i32,
+                            z:z as i32 -1
+                        }) == BlockID::Air {
+                            let new_quad: &mut [Vec3] = &mut Chunk::BACKQUAD.clone();
+                            for i in 0..4 {
+                                new_quad[i] += offset;
+                            }
+                            indices.extend_from_slice(&[0,3,1,0,2,3]);
+                            vertices.extend_from_slice(new_quad);
+                        }
+                        if self.get_block(IVec3 {
+                            x:x as i32,
+                            y:y as i32,
+                            z:z as i32 +1
+                        }) == BlockID::Air {
+                            let new_quad: &mut [Vec3] = &mut Chunk::FRONTQUAD.clone();
+                            for i in 0..4 {
+                                new_quad[i] += offset;
+                            }
+                            indices.extend_from_slice(&[0,3,1,0,2,3]);
+                            vertices.extend_from_slice(new_quad);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    pub fn get_block(&self, pos: IVec3) -> BlockID {
+        let chunk_index: IVec3 = IVec3 {
+            x: pos.x / Chunk::CHUNKSIZE as i32,
+            y: pos.y / Chunk::CHUNKSIZE as i32,
+            z: pos.z / Chunk::CHUNKSIZE as i32,
+        };
+        let block_index: IVec3 = IVec3 {
+            x: pos.x % Chunk::CHUNKSIZE as i32,
+            y: pos.y % Chunk::CHUNKSIZE as i32,
+            z: pos.z % Chunk::CHUNKSIZE as i32,
+        };
+        let chunk = self.map.get(&chunk_index);
+        match chunk {
+            Some(val) => {
+                val.data[block_index.x as usize][block_index.y as usize][block_index.z as usize]
+            },
+            None => {
+                BlockID::Air
+            }
+        }
+    }
+    pub fn get_block_from_index(&self, chunk_index: IVec3, block_index: UVec3) -> BlockID {
+        let chunk = self.map.get(&chunk_index);
+        match chunk {
+            Some(val) => {
+                val.data[block_index.x as usize][block_index.y as usize][block_index.z as usize]
+            },
+            None => {
+                BlockID::Air
+            }
+        }
+    }
+}
+
+pub fn process_chunks(mut commands: Commands, query: Query<&mut Chunk>) {
+    for mut chunk in query {
+        match chunk.state {
+            ChunkState::MeshDirty => {
+                chunk.state = ChunkState::Renderable;
+            }
+            ChunkState::MeshRendering => {
+                continue;
+            }
+            ChunkState::UnLoaded => {
+                todo!();
+                chunk.state = ChunkState::MeshDirty;
+            }
+            ChunkState::Renderable => {
+                todo!()
+            }
+        }
+    }
+}
